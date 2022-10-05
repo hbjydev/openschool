e8756fcc197f43d5f56a62b6db5ca327541d7794
Hayden Young <hayden@hbjy.dev>
2022-10-06 00:21:54 +0100

    feat: add better json error handling for page and perpage not being ints

--- services/class/handlers/classes/handler.go
+++ services/class/handlers/classes/handler.go
@@ -1,6 +1,8 @@
 package classes
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"go.h4n.io/openschool/services/class/repos/class"
 )
@@ -19,11 +21,34 @@ func NewClassesHandler(repository class.ClassRepository) ClassesHandler {
 func (h *ClassesHandler) RegisterRoutes(e *gin.Engine) {
 	group := e.Group("/classes")
 	group.GET("/", func(ctx *gin.Context) {
+		perPageStr := ctx.Query("perPage")
+		pageStr := ctx.Query("page")
 		req := ClassesIndexRequest{}
+
+		if perPageStr != "" {
+			perPage, err := strconv.Atoi(perPageStr)
+			if err != nil {
+				ctx.Error(err)
+				ctx.JSON(400, gin.H{"error": "perPage must be a number"})
+				return
+			}
+			req.PerPage = perPage
+		}
+
+		if pageStr != "" {
+			page, err := strconv.Atoi(pageStr)
+			if err != nil {
+				ctx.Error(err)
+				ctx.JSON(400, gin.H{"error": "page must be a number"})
+				return
+			}
+			req.Page = page
+		}
 
 		res, err := h.ClassesIndex(req)
 		if err != nil {
 			ctx.Error(err)
+			ctx.JSON(500, gin.H{"error": "something went wrong trying to retrieve the list of classes"})
 			return
 		}
 
